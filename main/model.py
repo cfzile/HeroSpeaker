b7885029cfc0d1f9ce8cbbed4dc872faad89d596
zile <cfzile@gmail.com>
2022-12-20 16:15:15 +0300

    add model

--- main/model.py
+++ main/model.py
@@ -9,9 +9,10 @@ from TTS.tts.utils.speakers import SpeakerManager
 CONFIG_SE_PATH = "speaker_encoders/se_1/config_se.json"
 CHECKPOINT_SE_PATH = "speaker_encoders/se_1/SE_checkpoint.pth.tar"
 
-OUT_PATH = 'out/'
+OUT_PATH = 'static/out/'
 os.makedirs(OUT_PATH, exist_ok=True)
 USE_CUDA = torch.cuda.is_available()
+
 
 class OurTTSModel:
 
@@ -21,42 +22,39 @@ class OurTTSModel:
         self.ap = AudioProcessor(**self.tts.synthesizer.tts_config.audio)
 
         self.tts.synthesizer.tts_model.length_scale = 1  # scaler for the duration predictor. The larger it is, the slower the speech.
-        self.tts.synthesizer.tts_model.inference_noise_scale = 0.3 # defines the noise variance applied to the random z vector at inference.
-        self.tts.synthesizer.tts_model.inference_noise_scale_dp = 0.3 # defines the noise variance applied to the duration predictor z vector at inference.
-
+        self.tts.synthesizer.tts_model.inference_noise_scale = 0.3  # defines the noise variance applied to the random z vector at inference.
+        self.tts.synthesizer.tts_model.inference_noise_scale_dp = 0.3  # defines the noise variance applied to the duration predictor z vector at inference.
 
     def load_speaker_encoder(self, CHECKPOINT_SE_PATH, CONFIG_SE_PATH):
-        self.SE_speaker_manager = SpeakerManager(encoder_model_path=CHECKPOINT_SE_PATH, 
-                                    encoder_config_path=CONFIG_SE_PATH, 
-                                    use_cuda=USE_CUDA)
+        self.SE_speaker_manager = SpeakerManager(encoder_model_path=CHECKPOINT_SE_PATH,
+                                                 encoder_config_path=CONFIG_SE_PATH,
+                                                 use_cuda=USE_CUDA)
 
-    def fit(self, paths, text):
+    def fit(self, paths, text, out_path):
         reference_files = list(paths)
         for sample in reference_files:
-            #!ffmpeg-normalize $sample -nt rms -t=-27 -o $sample -ar 16000 -f
+            # !ffmpeg-normalize $sample -nt rms -t=-27 -o $sample -ar 16000 -f
             os.system(f"ffmpeg-normalize {sample} -nt rms -t=-27 -o {sample} -ar 16000 -f")
 
         reference_emb = self.SE_speaker_manager.compute_embedding_from_clip(reference_files)
 
         wav, alignment, _, _ = synthesis(
-                    self.tts.synthesizer.tts_model,
-                    text,
-                    self.tts.synthesizer.tts_config,
-                    "cuda" in str(next(self.tts.synthesizer.tts_model.parameters()).device),
-                    speaker_id=None,
-                    d_vector=reference_emb,
-                    style_wav=None,
-                    language_id=0,
-                    use_griffin_lim=True,
-                    do_trim_silence=True,
-                ).values()
+            self.tts.synthesizer.tts_model,
+            text,
+            self.tts.synthesizer.tts_config,
+            "cuda" in str(next(self.tts.synthesizer.tts_model.parameters()).device),
+            speaker_id=None,
+            d_vector=reference_emb,
+            style_wav=None,
+            language_id=0,
+            use_griffin_lim=True,
+            do_trim_silence=True,
+        ).values()
 
-        file_name = text.replace(" ", "_")
-        file_name = file_name.translate(str.maketrans('', '', string.punctuation.replace('_', ''))) + '.wav'
-        out_path = os.path.join(OUT_PATH, file_name)
         print(" > Saving output to {}".format(out_path))
         self.ap.save_wav(wav, out_path)
 
+        return out_path
 
 
 our_tts = OurTTSModel()
--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from pydub import AudioSegment
 
 from . import constance, settings
-from . import model
+from .model import our_tts as model
 from .forms import UploadFileForm
 
 
@@ -23,10 +24,15 @@ def home(request):
 
 def load_data(request):
     if request.method == 'POST':
-        filename = '/static/tmp.mp3'
-        handle_uploaded_file(request.FILES['file'], settings.BASE_DIR.__str__() + filename)
+        audio_format = request.FILES['file'].name
+        audio_format = audio_format[audio_format.rfind('.') + 1:]
+        filename = f'static/audio/{request.POST["csrfmiddlewaretoken"]}'
+        handle_uploaded_file(request.FILES['file'], f'{filename}.{audio_format}')
+        sound = AudioSegment.from_file(f'{filename}.{audio_format}', f"{audio_format}")
+        sound.export(f'{filename}.wav', format="wav")
         text = request.POST.get('text')
-        file_path = filename
-        audio_path = model.fit(file_path, text)
+        file_path = f'{filename}.wav'
+        out_path = f'static/out/{request.POST["csrfmiddlewaretoken"]}.wav'
+        audio_path = model.fit([file_path], text, out_path)
         return render(request, 'pages/results.html',
-                      get_full_context(request, {'PAGE_NAME': constance.HOME_PAGE_NAME, 'audio_path': audio_path}))
+                      get_full_context(request, {'PAGE_NAME': constance.HOME_PAGE_NAME, 'audio_path': audio_path, 'text': text}))
